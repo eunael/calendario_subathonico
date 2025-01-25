53296319ce59054a71315a62db66bfe954e26da4
Natanael A. de Morais <natanaelallves@gmail.com>
2025-01-25 00:37:40 -0300

    Return erro from api

--- api/src/Controller/TimeController.php
+++ api/src/Controller/TimeController.php
@@ -8,48 +8,51 @@ use Carbon\Carbon;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
-
-use function Symfony\Component\Clock\now;
 
 final class TimeController extends AbstractController
 {
     #[Route('/time', name: 'app_time')]
     public function index(HttpClientInterface $httpClient, TimeRepository $timeRepository): JsonResponse
     {
-        $times = $timeRepository->findAll();
-        $time = empty($times) ? null : $times[0];
+        try {
+            $times = $timeRepository->findAll();
+            $time = empty($times) ? null : $times[0];
 
-        if ($time === null) {
-            $timeLeft = $httpClient->request('GET', 'https://subathon-api.justdavi.dev/api/time-left');
+            if ($time === null) {
+                $timeLeft = $httpClient->request('GET', 'https://subathon-api.justdavi.dev/api/time-left');
 
-            $currentTime = Carbon::now('America/Sao_Paulo')->getTimestampMs();
+                $currentTime = Carbon::now('America/Sao_Paulo')->getTimestampMs();
 
-            $finalTime = $currentTime + $timeLeft->toArray()['timeLeft'];
+                $finalTime = $currentTime + $timeLeft->toArray()['timeLeft'];
 
-            $time = new Time(
-                $finalTime,
-                Carbon::now('America/Sao_Paulo')->addDay()->hour(6)->toDateTimeString()
-            );
+                $time = new Time(
+                    $finalTime,
+                    Carbon::now('America/Sao_Paulo')->addDay()->hour(6)->toDateTimeString()
+                );
 
-            $timeRepository->add($time);
+                $timeRepository->add($time);
+
+                return $this->json($time);
+            } elseif (Carbon::now('America/Sao_Paulo')->isAfter(Carbon::parse($time->getTimeToUpdate()))) {
+                $timeLeft = 639358459;
+                $currentTime = Carbon::now('America/Sao_Paulo')->getTimestampMs();
+
+                $finalTime = strval($currentTime + $timeLeft);
+
+                $time->setTimestamp($finalTime);
+                $time->setTimeToUpdate(Carbon::now('America/Sao_Paulo')->addDay()->hour(6)->toDateTimeString());
+
+                $timeRepository->update();
+
+                return $this->json($time);
+            }
 
             return $this->json($time);
-        } elseif (Carbon::now('America/Sao_Paulo')->isAfter(Carbon::parse($time->getTimeToUpdate()))) {
-            $timeLeft = 639358459;
-            $currentTime = Carbon::now('America/Sao_Paulo')->getTimestampMs();
-
-            $finalTime = strval($currentTime + $timeLeft);
-
-            $time->setTimestamp($finalTime);
-            $time->setTimeToUpdate(Carbon::now('America/Sao_Paulo')->addDay()->hour(6)->toDateTimeString());
-
-            $timeRepository->update();
-
-            return $this->json($time);
+        } catch (\Throwable $th) {
+            return [
+                'error' => $th->getMessage()
+            ];
         }
-
-        return $this->json($time);
     }
 }
